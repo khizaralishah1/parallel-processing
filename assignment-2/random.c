e19826469fb0fd18549503959fd5592a24c2a69c
khizaralishahcs <khizaralishahcs07@gmail.com>
2023-11-16 19:22:39 +0500

    omp issue

--- assignment-2/random.c
+++ assignment-2/random.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main(int argc, char* argv[]) {
-    for(int i=0; i<argc; ++i) {
-        printf("%d ", random)
+int main(int argc, char *argv[])
+{
+    int max = atoi(argv[1]);
+
+    for (int i = 0; i < max; ++i)
+    {
+        printf("%d ", rand() % 100);
     }
+
+    return 0;
 }
